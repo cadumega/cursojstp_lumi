740ee4ea2e674ef85a7741f8cb9bc04da873a0d5
cadumega <72901445+cadumega@users.noreply.github.com>
2021-03-25 03:27:12 -0300

    intro_

---
+++ aula5/index.js
@@ -0,0 +1,8 @@
+console.log('Meu nome é "Carlos". Estou aprendendo Javascript às', 10, 'da manhã');
+
+ //teste comentário
+ console.log('Olá!');
+ console.log('Este trecho será exibido no console do navegador');
+
+
+
