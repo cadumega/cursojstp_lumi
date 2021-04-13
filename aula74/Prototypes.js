56eea57ad8d33afe8a6319feba8f696edefc749f
cadumega <72901445+cadumega@users.noreply.github.com>
2021-04-13 12:49:24 -0300

    prototype_instancia_referencia_fcconstrutora_metodos()

--- aula74/Prototypes.js
+++ aula74/Prototypes.js
@@ -1,15 +1,34 @@
+// Problema de performance, com repetições de funções e métodos dentro de um Objeto pessoa por exemplo.
+
 // Construtora -> molde (classe)  , para gerar novos objetos.
 function Pessoa(nome, sobrenome) {
   this.nome = nome;
   this.sobrenome = sobrenome;
-  this.nomeCompleto = () => this.nome + ' ' + this.sobrenome;
+  this.nomeCompleto = () => 'Original:' this.nome + ' ' + this.sobrenome;
 }
+
+
+//prototype linka com a instância, fará a mesma coisa caso crie outras variáveis.
+
+Pessoa.prototype.estouAqui = 'hahaha';
+
+
+//Objeto tem uma referência interna __proto___, que vem da fç prototypeda fç construtora. Procura primeiro dentro do objeto oficial , referência do pai dela. Todas as instâncias usarem esse método.
+
+Pessoa.prototype.nomeCompleto = function() {
+ return this.nome + ' ' + this.sobrenome;
+};
 
 //  Criando uma instância ,qd coloco new
 const pessoa1 = new Pessoa('Luiz', 'Otávio'); // objetos que vem da fç Pessoa = Fç construtora
-const pessoa2 = new Pessoa('Maria', 'A.');  // <-Pessoa = Fç construtora
-
+const pessoa2 = new Pessoa('Maria', 'A.'); // <-- Pessoa = Fç construtora
+const data = new Data(); // <-Date => Fç construtora
 
 console.dir(pessoa1);
-console.dir(pessoa2);
- 
+console.dir(data);
+
+// Os objetos utilizam o método do pai, que nem getFullYear().
+
+
+// Cadeia onde o motor do js irá buscar os métodos
+pessoa1 --> Pessoa.prototype --> Object.prototype
